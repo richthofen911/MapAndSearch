b36ec7fcb2aea0d1c797e2a4bc856b04780c4f53
richthofen911 <yichaoli.richthofen@gmail.com>
2018-06-18 22:30:12 -0400

    Finished.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MapAndSearch
+
+#### How to set up and running with the application
+1. Open the project in Android Studio.
+
+2. Follow the pop-up windows to set required Android SDK location/Gradle version/missing build-tools(if necessary).
+
+3. When Gradle build is finished and successful, click `Run` button on Android Studio's toolbar
+
+#### Expected display and result
+- The screen should show a search input field at the top and a map below it.
+- After a location is input and selected, the map's camera would move to that location based on its
+  latitude and longitude with an animation. A marker would be shown at the
+  exact place to indicate the location.
--- app/src/main/java/net/callofdroidy/mapandsearch/MainActivity.java
+++ app/src/main/java/net/callofdroidy/mapandsearch/MainActivity.java
@@ -1,13 +1,13 @@
 package net.callofdroidy.mapandsearch;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,14 +15,21 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    SupportPlaceAutocompleteFragment searchInputField;
-    SupportMapFragment worldMap;
+    private static final int DEFAULT_MAP_ZOOM_IN_LEVEL = 15;
+    private static final int COUNTRY_TYPE_MAP_ZOOM_IN_LEVEL = 4;
+    private static final int PROVINCE_TYPE_MAP_ZOOM_IN_LEVEL = 7;
+    private static final int CITY_TYPE_MAP_ZOOM_IN_LEVEL = 11;
+    private static final int COUNTY_TYPE_MAP_ZOOM_IN_LEVEL = 13;
 
-    GoogleMap myMap;
+    @Nullable
+    private GoogleMap myMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +37,57 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
-        searchInputField = (SupportPlaceAutocompleteFragment) fragmentManager.findFragmentById(R.id.search_input_field);
-        worldMap = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map);
 
-        if (worldMap != null) {
+        SupportPlaceAutocompleteFragment searchInputField =
+                (SupportPlaceAutocompleteFragment) fragmentManager.findFragmentById(R.id.search_input_field);
+        SupportMapFragment worldMap = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map);
+
+        if (worldMap != null && searchInputField != null) {
+
             worldMap.getMapAsync(this);
-        }
+            searchInputField.setHint(getString(R.string.search_input_field_hint));
 
-        searchInputField.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                Log.e(TAG, "onPlaceSelected: " + place.getName());
-                if (myMap != null) {
-                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 10));
+            searchInputField.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    Log.i(TAG, "onPlaceSelected: " + place.getName() + ", " + place.getPlaceTypes());
+                    if (myMap != null) {
+                        // clear the marker for the previous location
+                        myMap.clear();
+
+                        // adjust the zoom level based if the location is a country/province/city/county
+                        float zoom_level = DEFAULT_MAP_ZOOM_IN_LEVEL;
+                        List<Integer> locationType = place.getPlaceTypes();
+                        if (locationType.contains(Place.TYPE_COUNTRY)) {
+                            zoom_level = COUNTRY_TYPE_MAP_ZOOM_IN_LEVEL;
+                        } else if (locationType.contains(Place.TYPE_ADMINISTRATIVE_AREA_LEVEL_1) ||
+                                locationType.contains(Place.TYPE_ADMINISTRATIVE_AREA_LEVEL_2) ||
+                                locationType.contains(Place.TYPE_ADMINISTRATIVE_AREA_LEVEL_3)) {
+                            zoom_level = PROVINCE_TYPE_MAP_ZOOM_IN_LEVEL;
+                        } else if (locationType.contains(Place.TYPE_LOCALITY)) {
+                            zoom_level = CITY_TYPE_MAP_ZOOM_IN_LEVEL;
+                        } else if (locationType.contains(Place.TYPE_SUBLOCALITY) ||
+                                locationType.contains(Place.TYPE_SUBLOCALITY_LEVEL_1) ||
+                                locationType.contains(Place.TYPE_SUBLOCALITY_LEVEL_2) ||
+                                locationType.contains(Place.TYPE_SUBLOCALITY_LEVEL_3) ||
+                                locationType.contains(Place.TYPE_SUBLOCALITY_LEVEL_4) ||
+                                locationType.contains(Place.TYPE_SUBLOCALITY_LEVEL_5)) {
+                            zoom_level = COUNTY_TYPE_MAP_ZOOM_IN_LEVEL;
+                        }
+
+                        LatLng location = place.getLatLng();
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), zoom_level));
+                        // add marker to indicate the selected location
+                        myMap.addMarker(new MarkerOptions().position(location));
+                    }
                 }
-            }
 
-            @Override
-            public void onError(Status status) {
-                Log.e(TAG, "onPlaceError: " + status.getStatusMessage());
-            }
-        });
+                @Override
+                public void onError(Status status) {
+                    Log.e(TAG, "onPlaceError: " + status.getStatusMessage());
+                }
+            });
+        }
     }
 
     @Override
